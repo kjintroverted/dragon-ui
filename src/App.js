80aa8e1a62346991e9d952586877d08ab3e5d2fd
Clayton <clayton.yarborough@gmail.com>
2019-05-25 12:02:05 -0400

    eslinting tweaks

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App ">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" /> 
-        <Welcome/>
+        <img src={logo} className="App-logo" alt="logo" />
+        <Welcome />
       </header>
     </div>
   );
