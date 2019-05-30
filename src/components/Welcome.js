8ade40b2db8d2f08f4f430634e613c5b3362c137
Clayton <clayton.yarborough@gmail.com>
2019-05-29 20:51:53 -0400

    adds in comments to explain react-hook example

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,15 +4,25 @@ import DungeonService from '../services/dungeonService';
 
 
 function Welcome() {
+  // [Variable in State, Setter of Variable] 
+  // hooks are indicated by the word `use` (linter looks for use keyword)
   const [welcome, setWelcome] = useState('Just a moment...');
 
+  // Effects run immediately after render (including the first render)
+  // Don't be tempted to make useEffect async console will scream at you
   useEffect(()=>{
+    // Async calls are to be made inside useEffect then immediately called below
    const getWelcome = async ()=>{
       let welcomeServer = await DungeonService.getWelcome();
        setWelcome(welcomeServer);
       };
       getWelcome();
-  },[])
+
+  // Array at the end is requried to avoid looping
+  // indicates that this is only triggered on mount
+  // if we put [propVariable] inside it would trigger again on propVariable change
+  // See https://www.robinwieruch.de/react-hooks-fetch-data/ for indepth material
+  },[]);
      
     return (
           <div>
