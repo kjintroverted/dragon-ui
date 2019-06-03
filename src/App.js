8acfaa162f335735646670a0207fdba38f18ec82
Williamrobert Green <wkgreen13@gmail.com>
2019-06-03 13:31:48 -0400

    adds content container

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
 import Welcome from './components/Welcome';
-import './App.css';
 import CharacterBackground from './components/CharacterBackground';
+
+import './App.css';
 
 function App() {
   return (
     <div className="App ">
       <Welcome />
-      <CharacterBackground />
+      <Content>
+        <CharacterBackground />
+      </Content>
     </div>
   );
 }
 
 export default App;
+
+const Content = styled.div`
+  display: flex;
+`;
