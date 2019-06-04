e0e1ed98cc295d9af174c441089592519ba64923
Clayton <clayton.yarborough@gmail.com>
2019-06-04 08:15:28 -0400

    adds Owner view component WIP

---
+++ src/components/OwnerView.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+import DungeonService from '../services/dungeonService';
+
+function OwnerView() {
+  return null;
+}
+
+export default OwnerView;
