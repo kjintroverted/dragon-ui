ed5f219f2644247fa3323dd90920f6c2b09e99ce
Williamrobert Green <wkgreen13@gmail.com>
2019-06-07 14:32:07 -0400

    adds prop-types

--- src/components/OwnerView.js
+++ src/components/OwnerView.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 import DungeonService from '../services/dungeonService';
@@ -27,6 +28,10 @@ function OwnerView({ owner }) {
 }
 
 export default OwnerView;
+
+OwnerView.propTypes = {
+  owner: PropTypes.string.isRequired,
+};
 
 const Container = styled.div`
     display:flex;
