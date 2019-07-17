323701a0eee453d69d78bdd9e62a493ed3c8a705
Williamrobert Green <wkgreen13@gmail.com>
2019-07-17 10:38:32 -0400

    adds some common styled components

---
+++ src/components/CustomStyled.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const Spacer = styled.span`
+  flex: 1;
+`;
+
+export const Card = styled.div`
+  position: relative;
+  background: white;
+  box-shadow: lightgray 1px 1px 5px;
+  border-radius: 5px;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ActionBar = styled.span`
+  display: flex;
+  flex-direction: row-reverse;
+`;
+
+export const HeaderBar = styled.div`
+  display: flex;
+  align-items: center;
+  border-bottom: lightgray solid 1px;
+`;
+
+export const Row = styled.div`
+display: flex
+`;
