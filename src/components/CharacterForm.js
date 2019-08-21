e7465212f4804ea0947f2a7266af31471560ed13
kjintroverted <wkgreen13@gmail.com>
2019-08-21 14:19:57 -0400

    moves character form to comp

---
+++ src/components/CharacterForm.js
@@ -0,0 +1,157 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import { Card, Spacer, HeaderBar, ActionBar, BasicBox } from "./CustomStyled";
+import {
+  IconButton,
+  FormControl,
+  FormLabel,
+  Select,
+  MenuItem,
+  Divider,
+  FormControlLabel,
+  Checkbox,
+  TextField,
+  OutlinedInput
+} from "@material-ui/core";
+import { skillsArray } from "../services/helper";
+
+const CharacterForm = ({ races, classes, save }) => {
+  const [values, setValues] = useState({});
+
+  function handleValueChange(field, numeric) {
+    return e =>
+      setValues({
+        ...values,
+        [field]: numeric ? +e.target.value : e.target.value
+      });
+  }
+
+  function raceSelect(e) {
+    const race = races.find(r => r.name === e.target.value);
+    setValues({ ...values, race: race.name, speed: race.speed });
+  }
+
+  function toggleSKill(e) {
+    const arr = values.proSkills || [];
+    const i = arr.findIndex(skill => skill === e.target.value);
+    if (i === -1) setValues({ ...values, proSkills: [...arr, e.target.value] });
+    else
+      setValues({
+        ...values,
+        proSkills: [...arr.slice(0, i), ...arr.slice(i + 1)]
+      });
+  }
+
+  async function addCharacter() {
+    setValues({});
+    save(values);
+  }
+
+  return (
+    <Card>
+      <HeaderBar>
+        <h4>New Character</h4>
+        <Spacer />
+        <ActionBar>
+          <IconButton onClick={addCharacter}>
+            <i className='material-icons'>done</i>
+          </IconButton>
+        </ActionBar>
+      </HeaderBar>
+      <InfoRow>
+        <TextField
+          variant='outlined'
+          label='Name'
+          onChange={handleValueChange("name")}
+        />
+        <BasicBox>
+          <TextField
+            variant='outlined'
+            label='HP'
+            type='number'
+            onChange={handleValueChange("maxHP", true)}
+          />
+        </BasicBox>
+      </InfoRow>
+      <InfoRow>
+        <FormControl variant='outlined' style={{ minWidth: 120 }}>
+          <FormLabel htmlFor='race'>Race</FormLabel>
+          <Select
+            value={values.race || ""}
+            onChange={raceSelect}
+            input={<OutlinedInput id='race' />}
+          >
+            {races.map(val => (
+              <MenuItem key={val.name} value={val.name}>
+                {val.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <Spacer />
+        <FormControl variant='outlined' style={{ minWidth: 120 }}>
+          <FormLabel htmlFor='class'>Class</FormLabel>
+          <Select
+            value={values.class || ""}
+            onChange={handleValueChange("class")}
+            input={<OutlinedInput id='class' />}
+          >
+            {classes.map(val => (
+              <MenuItem key={val.name} value={val.name}>
+                {val.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </InfoRow>
+      <Divider />
+      <HeaderBar>
+        <p>
+          <strong>Proficient Skills</strong>
+        </p>
+      </HeaderBar>
+      <SkillSelect>
+        {skillsArray.map(({ label }) => (
+          <FormControlLabel
+            key={label}
+            control={
+              <Checkbox
+                checked={
+                  (values.proSkills &&
+                    values.proSkills.indexOf(label) !== -1) ||
+                  false
+                }
+                value={label}
+                onChange={toggleSKill}
+                color='primary'
+              />
+            }
+            label={label}
+          />
+        ))}
+      </SkillSelect>
+    </Card>
+  );
+};
+
+export default CharacterForm;
+
+CharacterForm.propTypes = {
+  save: PropTypes.func.isRequired,
+  races: PropTypes.array.isRequired,
+  classes: PropTypes.array.isRequired
+};
+
+const InfoRow = styled.div`
+  display: flex;
+  margin-top: 0.62em;
+`;
+
+const SkillSelect = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 150px);
+  grid-gap: 10px;
+  margin: 10px 0px;
+  justify-content: center;
+`;
--- src/containers/OwnerView.js
+++ src/containers/OwnerView.js
@@ -1,23 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
 
-import {
-  Fab, TextField, IconButton, Select, MenuItem, OutlinedInput, FormControl, FormLabel, FormControlLabel, Checkbox, Divider,
-} from '@material-ui/core';
-import { Link } from 'react-router-dom';
-import CharacterSummary from '../components/CharacterSummary';
-import DungeonService from '../services/dungeonService';
-import {
-  BottomAnchor, TopAnchor, HeaderBar, ActionBar, Card, Spacer, BasicBox,
-} from '../components/CustomStyled';
-import { skillsArray } from '../services/helper';
+import { Fab } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import CharacterSummary from "../components/CharacterSummary";
+import CharacterForm from "../components/CharacterForm";
+import DungeonService from "../services/dungeonService";
+import { BottomAnchor, TopAnchor } from "../components/CustomStyled";
 
 function OwnerView({ owner }) {
   const [characters, updateCharacters] = useState([]);
   const [classes, setClasses] = useState([]);
   const [races, setRaces] = useState([]);
-  const [values, setValues] = useState({});
   const [party, updateParty] = useState([]);
   const [isAdding, setAdding] = useState(false);
 
@@ -39,26 +34,8 @@ function OwnerView({ owner }) {
     setClasses(classList);
   }
 
-  function handleValueChange(field, numeric) {
-    return e => setValues({ ...values, [field]: numeric ? +e.target.value : e.target.value });
-  }
-
-  function raceSelect(e) {
-    const race = races.find(r => r.name === e.target.value);
-    setValues({ ...values, race: race.name, speed: race.speed });
-  }
-
-  function toggleSKill(e) {
-    const arr = values.proSkills || [];
-    const i = arr.findIndex(skill => skill === e.target.value);
-    if (i === -1) setValues({ ...values, proSkills: [...arr, e.target.value] });
-    else setValues({ ...values, proSkills: [...arr.slice(0, i), ...arr.slice(i + 1)] });
-  }
-
-  async function addCharacter() {
-    const data = { ...values, owner };
-    await DungeonService.saveCharacter(data);
-    setValues({});
+  async function addCharacter(data) {
+    await DungeonService.saveCharacter({ ...data, owner });
     setAdding(false);
     getCharactersByOwner();
   }
@@ -73,7 +50,7 @@ function OwnerView({ owner }) {
   }, [isAdding]);
 
   const characterCards = [];
-  characters.forEach((character) => {
+  characters.forEach(character => {
     characterCards.push(
       <CharacterSummary
         key={character.id}
@@ -81,93 +58,34 @@ function OwnerView({ owner }) {
         highlight={party.indexOf(character.id) !== -1}
         add={() => toggleCharacter(character.id)}
         linkTo={`/character?id=${character.id}`}
-      />,
+      />
     );
   });
 
   return (
     <Container>
       <TopAnchor>
-        <Fab size="small" color="secondary" onClick={() => setAdding(!isAdding)}>
-          <i className="material-icons">{ !isAdding ? 'add' : 'close' }</i>
+        <Fab
+          size='small'
+          color='secondary'
+          onClick={() => setAdding(!isAdding)}
+        >
+          <i className='material-icons'>{!isAdding ? "add" : "close"}</i>
         </Fab>
       </TopAnchor>
-      { characterCards }
-      { isAdding
-        && <Card>
-          <HeaderBar>
-            <h4>New Character</h4>
-            <Spacer />
-            <ActionBar>
-              <IconButton onClick={addCharacter}>
-                <i className="material-icons">done</i>
-              </IconButton>
-            </ActionBar>
-          </HeaderBar>
-          <InfoRow>
-            <TextField variant="outlined" label="Name" onChange={handleValueChange('name')} />
-            <BasicBox>
-              <TextField variant="outlined" label="HP" type="number" onChange={handleValueChange('maxHP', true)} />
-            </BasicBox>
-          </InfoRow>
-          <InfoRow>
-            <FormControl variant="outlined" style={{ minWidth: 120 }}>
-              <FormLabel htmlFor="race">Race</FormLabel>
-              <Select
-                value={values.race || ''}
-                onChange={raceSelect}
-                input={<OutlinedInput id="race" />}
-              >
-                {
-                  races.map(val => <MenuItem key={val.name} value={val.name}>{ val.name }</MenuItem>)
-                }
-              </Select>
-            </FormControl>
-            <Spacer />
-            <FormControl variant="outlined" style={{ minWidth: 120 }}>
-              <FormLabel htmlFor="class">Class</FormLabel>
-              <Select
-                value={values.class || ''}
-                onChange={handleValueChange('class')}
-                input={<OutlinedInput id="class" />}
-              >
-                {
-                  classes.map(val => <MenuItem key={val.name} value={val.name}>{ val.name }</MenuItem>)
-                }
-              </Select>
-            </FormControl>
-          </InfoRow>
-          <Divider />
-          <HeaderBar>
-            <p><strong>Proficient Skills</strong></p>
-          </HeaderBar>
-          <SkillSelect>
-            { skillsArray.map(({ label }) => (
-              <FormControlLabel
-                key={label}
-                control={
-                  <Checkbox
-                    checked={(values.proSkills && values.proSkills.indexOf(label) !== -1) || false}
-                    value={label}
-                    onChange={toggleSKill}
-                    color="primary"
-                  />
-                }
-                label={label}
-              />
-            )) }
-          </SkillSelect>
-           </Card>
-      }
-      { !!party.length
-        && <BottomAnchor>
+      {characterCards}
+      {isAdding && (
+        <CharacterForm races={races} classes={classes} save={addCharacter} />
+      )}
+      {!!party.length && (
+        <BottomAnchor>
           <Link to={`/character?id=${party.join()}`} style={{ zIndex: 10 }}>
-            <Fab color="secondary">
-              <i className="material-icons">group</i>
+            <Fab color='secondary'>
+              <i className='material-icons'>group</i>
             </Fab>
           </Link>
-           </BottomAnchor>
-      }
+        </BottomAnchor>
+      )}
     </Container>
   );
 }
@@ -175,27 +93,14 @@ function OwnerView({ owner }) {
 export default OwnerView;
 
 OwnerView.propTypes = {
-  owner: PropTypes.string.isRequired,
+  owner: PropTypes.string.isRequired
 };
 
 const Container = styled.div`
-        position: relative;
-        width: 100vw;
-        display: grid;
-        grid-template-columns: repeat(auto-fill, 22em);
-        grid-gap: .625em;
-        justify-content: center;
-    `;
-
-const InfoRow = styled.div`
-  display: flex;
-  margin-top: .62em;
-`;
-
-const SkillSelect = styled.div`
+  position: relative;
+  width: 100vw;
   display: grid;
-  grid-template-columns: repeat(auto-fill, 150px);
-  grid-gap: 10px;
-  margin: 10px 0px;
+  grid-template-columns: repeat(auto-fill, 22em);
+  grid-gap: 0.625em;
   justify-content: center;
 `;
