2299d121f2531b30aba36fa23df3b1f6ad585511
Clayton <clayton.yarborough@gmail.com>
2019-12-23 09:01:12 -0500

    Adds MVP version of Death Saving Throws

---
+++ src/components/DeathSavingThrows.js
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import Checkbox from '@material-ui/core/Checkbox';
+import { withStyles } from '@material-ui/core/styles';
+import { green, red } from '@material-ui/core/colors';
+import styled from 'styled-components';
+import { Row } from './CustomStyled';
+
+const DeathSavingThrows = () => {
+  const [checked, setChecked] = useState({
+    succeess1: false,
+    succeess2: false,
+    succeess3: false,
+    failure1: false,
+    failure2: false,
+    failure3: false,
+  });
+
+  const handleChange = (event) => {
+    setChecked({ ...checked, [event.target.name]: event.target.checked });
+  };
+
+  return (
+      <SavingThrowsContainer>
+          <h2 style={{ margin: 0 }}>Death Saving Throws</h2>
+        <Row style={{ justifyContent: 'flex-end' }}>
+            <h3 style={{ margin: 0 }}>Success</h3>
+            <GreenCheckbox
+              checked={checked.succeess1.checked}
+              onChange={handleChange}
+              name="success1"
+              value="primary"
+              inputProps={{ 'aria-label': 'primary checkbox' }}
+            />
+            <GreenCheckbox
+              checked={checked.succeess2.checked}
+              onChange={handleChange}
+              name="success2"
+              value="primary"
+              inputProps={{ 'aria-label': 'primary checkbox' }}
+            />
+            <GreenCheckbox
+              checked={checked.succeess3.checked}
+              onChange={handleChange}
+              name="success3"
+              value="primary"
+              inputProps={{ 'aria-label': 'primary checkbox' }}
+            />
+        </Row>
+        <Row style={{ justifyContent: 'flex-end' }}>
+            <h3 style={{ margin: 0 }}>Failure</h3>
+            <RedCheckbox
+              checked={checked.failure1.checked}
+              onChange={handleChange}
+              name="failure1"
+              value="primary"
+              inputProps={{ 'aria-label': 'primary checkbox' }}
+            />
+            <RedCheckbox
+              checked={checked.failure2.checked}
+              onChange={handleChange}
+              name="failure2"
+              value="primary"
+              inputProps={{ 'aria-label': 'primary checkbox' }}
+            />
+            <RedCheckbox
+              checked={checked.failure3.checked}
+              onChange={handleChange}
+              name="failure3"
+              value="primary"
+              inputProps={{ 'aria-label': 'primary checkbox' }}
+            />
+        </Row>
+      </SavingThrowsContainer>
+  );
+};
+
+export default DeathSavingThrows;
+
+const SavingThrowsContainer = styled.div`
+  margin: 3px 0px;
+  font-size: .8em;
+`;
+
+const GreenCheckbox = withStyles({
+  root: {
+    '&$checked': {
+      color: green[600],
+    },
+  },
+  checked: {},
+})(props => <Checkbox color="default" {...props} />);
+
+const RedCheckbox = withStyles({
+  root: {
+    '&$checked': {
+      color: red[600],
+    },
+  },
+  checked: {},
+})(props => <Checkbox color="default" {...props} />);
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Badge, TextField, Chip, IconButton } from '@material-ui/core';
+import {
+  Badge, TextField, Chip, IconButton,
+} from '@material-ui/core';
 import {
   Card, Column, Row, BasicBox, Spacer, HeaderBar,
 } from './CustomStyled';
+import DeathSavingThrows from './DeathSavingThrows';
 import { calculateModifier } from '../services/helper';
 
 const Profile = ({
   character, hitDice, update, disabled, editing,
 }) => {
-
   const [values, setValues] = useState({});
 
   function onValueChange(field) {
@@ -28,15 +30,15 @@ const Profile = ({
     return () => {
       const array = character[field] || [];
       update({ ...character, [field]: [...array, values[valueField]] });
-      setValues({ ...values, [valueField]: "" });
+      setValues({ ...values, [valueField]: '' });
     };
   }
 
   function remove(field, i) {
     return () => {
-      let array = [
+      const array = [
         ...character[field].slice(0, i),
-        ...character[field].slice(i + 1)
+        ...character[field].slice(i + 1),
       ];
       update({ ...character, [field]: array });
     };
@@ -44,20 +46,20 @@ const Profile = ({
 
   return (
     <Card>
-      <Row style={ { alignItems: 'center', justifyContent: 'flex-end' } }>
+      <Row style={{ alignItems: 'center', justifyContent: 'flex-end' }}>
         <Column>
           { !editing
-            ? <h2 style={ { margin: 0 } }>{ character.name }</h2>
+            ? <h2 style={{ margin: 0 }}>{ character.name }</h2>
             : <TextField
               label="Name"
-              value={ character.name }
-              onChange={ onChange('name') }
+              value={character.name}
+              onChange={onChange('name')}
             />
           }
-          <p style={ { margin: 0 } }>{ character.race } { character.class }</p>
+          <p style={{ margin: 0 }}>{ character.race } { character.class }</p>
         </Column>
         <Spacer />
-        <Badge badgeContent={ `+${ character.proBonus }` } color="secondary">
+        <Badge badgeContent={`+${character.proBonus}`} color="secondary">
           <BasicBox>
             { !editing
               ? <TextField
@@ -65,14 +67,14 @@ const Profile = ({
                 disabled
                 type="number"
                 label="Level"
-                value={ character.level }
+                value={character.level}
               />
               : <TextField
                 variant="outlined"
                 type="number"
                 label="XP"
-                value={ character.xp }
-                onChange={ onChange('xp', true) }
+                value={character.xp}
+                onChange={onChange('xp', true)}
               />
             }
           </BasicBox>
@@ -81,118 +83,124 @@ const Profile = ({
           { !editing
             ? <TextField
               variant="outlined"
-              disabled={ disabled }
+              disabled={disabled}
               type="number"
-              label={ `HP/${ character.maxHP }` }
-              value={ character.hp }
-              helperText={ `${ hitDice }` }
-              onChange={ onChange('hp', true) }
+              label={`HP/${character.maxHP}`}
+              value={character.hp}
+              helperText={`${hitDice}`}
+              onChange={onChange('hp', true)}
             />
             : <TextField
               variant="outlined"
-              disabled={ disabled }
+              disabled={disabled}
               type="number"
               label="Max HP"
-              value={ character.maxHP }
-              helperText={ `${ hitDice }` }
-              onChange={ onChange('maxHP', true) }
+              value={character.maxHP}
+              helperText={`${hitDice}`}
+              onChange={onChange('maxHP', true)}
             />
           }
         </BasicBox>
         <BasicBox>
           <TextField
             variant="outlined"
-            disabled={ !editing }
+            disabled={!editing}
             type="number"
             label="AC"
-            value={ character.armor }
-            onChange={ onChange('armor', true) }
+            value={character.armor}
+            onChange={onChange('armor', true)}
           />
         </BasicBox>
         <BasicBox>
           <TextField
             variant="outlined"
-            disabled={ !editing }
+            disabled={!editing}
             type="number"
             label="Speed"
-            value={ character.speed }
-            onChange={ onChange('speed', true) }
+            value={character.speed}
+            onChange={onChange('speed', true)}
           />
         </BasicBox>
-        <Badge badgeContent={ calculateModifier(character.dex) } color="secondary">
+        <Badge badgeContent={calculateModifier(character.dex)} color="secondary">
           <BasicBox>
             <TextField
               variant="outlined"
-              disabled={ disabled }
+              disabled={disabled}
               type="number"
               label="Init"
-              value={ character.initiative || '' }
-              onChange={ onChange('initiative', true) }
+              value={character.initiative || ''}
+              onChange={onChange('initiative', true)}
             />
           </BasicBox>
         </Badge>
       </Row>
       {/* LANGUAGES */ }
-      { editing && !disabled ?
+      { editing && !disabled
         // ADD NEW LANGUAGES
-        <>
-          <HeaderBar style={ { marginTop: '1em' } }>
+        ? <>
+          <HeaderBar style={{ marginTop: '1em' }}>
             Known Languages
           </HeaderBar>
           <Row>
-            { character.languages &&
-              character.languages.map((lang, i) => (
+            { character.languages
+              && character.languages.map((lang, i) => (
                 <Chip
-                  key={ `pro-${ lang }` }
-                  label={ lang }
-                  onDelete={ editing && !disabled ? remove("languages", i) : null }
-                  color='primary'
+                  key={`pro-${lang}`}
+                  label={lang}
+                  onDelete={editing && !disabled ? remove('languages', i) : null}
+                  color="primary"
                 />
               )) }
 
             <TextField
-              label='New Language'
-              value={ values.newLang || "" }
-              onChange={ onValueChange("newLang") }
+              label="New Language"
+              value={values.newLang || ''}
+              onChange={onValueChange('newLang')}
             />
-            <IconButton color='primary' onClick={ add("languages", "newLang") }>
-              <i className='material-icons'>done</i>
+            <IconButton color="primary" onClick={add('languages', 'newLang')}>
+              <i className="material-icons">done</i>
             </IconButton>
           </Row>
-        </> :
-        // DISPLAY LANGUAGES
-        <Info><b>Known Languages:</b> { character.languages && character.languages.length ? character.languages.join() : <i>none</i> }</Info>
+          </>
+        // DISPLAY LANGUAGES/TOOLS AND SAVING THROWS
+        : <Row style={{ justifyContent: 'space-between' }}>
+            <Column>
+              <Info><b>Known Languages:</b> { character.languages && character.languages.length ? character.languages.join() : <i>none</i> }</Info>
+              <Info><b>Tools Proficiencies:</b> { character.proTools && character.proTools.length ? character.proTools.join() : <i>none</i> }</Info>
+            </Column>
+           <Column>
+              <DeathSavingThrows />
+           </Column>
+          </Row>
       }
       {/* TOOLS */ }
-      { editing && !disabled ?
+      { editing && !disabled
         // ADD NEW TOOLS
-        <>
-          <HeaderBar style={ { marginTop: '1em' } }>
+        && <>
+          <HeaderBar style={{ marginTop: '1em' }}>
             Tool Proficiencies
           </HeaderBar>
           <Row>
-            { character.proTools &&
-              character.proTools.map((tool, i) => (
+            { character.proTools
+              && character.proTools.map((tool, i) => (
                 <Chip
-                  key={ `pro-${ tool }` }
-                  label={ tool }
-                  onDelete={ editing && !disabled ? remove("proTools", i) : null }
-                  color='primary'
+                  key={`pro-${tool}`}
+                  label={tool}
+                  onDelete={editing && !disabled ? remove('proTools', i) : null}
+                  color="primary"
                 />
               )) }
 
             <TextField
-              label='New Tool'
-              value={ values.newTool || "" }
-              onChange={ onValueChange("newTool") }
+              label="New Tool"
+              value={values.newTool || ''}
+              onChange={onValueChange('newTool')}
             />
-            <IconButton color='primary' onClick={ add("proTools", "newTool") }>
-              <i className='material-icons'>done</i>
+            <IconButton color="primary" onClick={add('proTools', 'newTool')}>
+              <i className="material-icons">done</i>
             </IconButton>
           </Row>
-        </> :
-        // DISPLAY TOOLS
-        <Info><b>Tools Proficiencies:</b> { character.proTools && character.proTools.length ? character.proTools.join() : <i>none</i> }</Info>
+           </>
       }
     </Card>
   );
@@ -215,4 +223,4 @@ Profile.propTypes = {
 const Info = styled.p`
   margin: 3px 0px;
   font-size: .8em;
-`
+`;
