e0edce69ffa22379133db606b4eaf106775b4b51
Clayton <clayton.yarborough@gmail.com>
2020-01-07 11:04:23 -0500

    remvoed ling

--- src/components/PartyChip.js
+++ src/components/PartyChip.js
@@ -9,9 +9,7 @@ function PartyChip({ name, members }) {
     e.preventDefault();
     const parties = JSON.parse(localStorage.getItem('parties'));
     // Grab index of the existing party
-    let index = -1;
-    index = parties.findIndex(savedParty => savedParty.name === name);
-    // Should always come back but just in case
+    const index = parties.findIndex(savedParty => savedParty.name === name);
     if (index !== -1) {
       parties.splice(index, 1);
     }
